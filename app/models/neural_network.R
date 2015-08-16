fa19498d32ef8e36be92510bc429744407f4dc05
Luis Capelo <luiscape@gmail.com>
2015-08-15 21:59:01 -0400

    linting

--- app/models/neural_network.R
+++ app/models/neural_network.R
@@ -9,18 +9,18 @@
 # Author: Luis Capelo luiscape@gmail.com
 #
 
-library(MASS)
+# library(MASS)
 library(forecast)
 
 
 FitNeuralNetworkModel <- function(data=NULL, production_model=TRUE, station_id=433, forecast_minutes=1440, train_set_size=.80) {
-  
+
   #
   # Load data.
   #
   cat(paste0('Calculating Neural Networks model for station id ', station_id, ' ...'))
   data <- filter(data, id == station_id)
-  
+
   #
   # Hack to filter the data from the beginning of
   # a day in the database.
@@ -33,21 +33,21 @@ FitNeuralNetworkModel <- function(data=NULL, production_model=TRUE, station_id=4
     if (i == 1) minutes_array <- m
     else minutes_array <- c(minutes_array, m)
   }
-  
+
   #
   # Organizing the model data.frame
   #
   model_data = data$availableBikesRatio
   time_series_data <- ts(model_data, start=1, frequency=1440)
 
-  
+
   #
   # Making the model.
   #
   if (production_model) {
     fit = nnetar(time_series_data, repeats=25)
     forecast_data <- forecast(fit, h=1440)
-    
+
     cat(' done.\n')
     df_output <- data.frame(
       a = station_id,
@@ -59,7 +59,7 @@ FitNeuralNetworkModel <- function(data=NULL, production_model=TRUE, station_id=4
     row.names(df_output) <- NULL
     return(df_output)
   }
-  
+
   #
   # Returning the model for train sets only.
   #
@@ -70,9 +70,9 @@ FitNeuralNetworkModel <- function(data=NULL, production_model=TRUE, station_id=4
     bikes_neural_nets <- nnetar(train_data)
     forecast_bikes <- forecast(bikes_neural_nets, h=forecast_minutes)
     neural_net_rmse <- accuracy(forecast_bikes, test_data)[,2]
-    
+
     cat(' done.\n')
-    return(data.frame(neural_net_rmse))  
+    return(data.frame(neural_net_rmse))
   }
 }
 
