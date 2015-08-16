0eb30ac9b787bc99de1a4b5d2928defbefc76f96
Luis Capelo <luiscape@gmail.com>
2015-08-15 21:58:50 -0400

    loading data directly from within the forecast model

--- app/models/auto_arima.R
+++ app/models/auto_arima.R
@@ -4,55 +4,81 @@
 # Author: Luis Capelo luiscape@gmail.com
 #
 
-library(MASS)
-library(dplyr)
+# library(MASS)
+# library(dplyr)
 library(forecast)
 
 #
 # Loading helper functions.
 #
-source('scripts/R/helpers/read_table.R')
+source(paste0(getwd(), '/app/helpers/read_station_day.R'))
 
-FitArimaModel <- function(data=NULL, production_model=FALSE, station_id=445, forecast_minutes=120, train_set_size=.80) {
+#' @get /forecast
+FitArimaModel <- function(data=NULL, production_model=TRUE, station=445, forecast_minutes=120, train_set_size=.80) {
 
-  #
-  # Load data.
-  #
+
   cat('Calculating ARIMA model ...')
-  # data <- ReadTable('station_processed', deploy=FALSE)
-  data <- filter(data, id == station_id)
-  
   #
-  # Hack to filter the data from the beginning of
-  # a day in the database.
+  # Load data from DB.
+  # Needs to be improved: only load
+  # data that matches a single day.
   #
-  data <- filter(data, as.POSIXct(executionTime) >= as.POSIXct('2015-06-20'))
-  
+  data <- ReadStationDay(day='2015-07-06', station_id=station)
+
   #
   # Organizing the model data.frame
   #
   model_data = data$availableBikesRatio
+  last_minute <- max(as.POSIXct(data$executionTime))
   time_series_data <- ts(model_data, start=1, frequency=1440)
-  
+
 
   if (production_model) {
     
+    FillTimes <- function(data=NULL) {
+      minutes = c()
+      for (i in 1:forecast_minutes) {
+        minute = format(last_minute + (i * 60), '%Y-%m-%d %H:%M')
+        minutes = c(minutes, minute)
+      }
+      return(
+        data.frame(
+          station = station, 
+          availableBikesRatio = data$"Point Forecast",
+          time = minutes
+        )
+      )
+    }
+
     #
     # Making the model.
     #
     bikes_arima <- auto.arima(time_series_data)
-    forecast_bikes <- forecast.Arima(bikes_arima, h=forecast_minutes)
+    elapsed_time <- system.time(
+      forecast_bikes <- forecast.Arima(bikes_arima, h=forecast_minutes)
+      )[3]
 
     #
     # Making plot
     #
     cat(' done.\n')
-    return(forecast_bikes)
-  }
 
+    output <- list(
+      success = TRUE,
+      meta = data.frame(
+        object = "availableBikesRatio",
+        minutes = forecast_minutes,
+        model = "ARIMA",
+        algorithm = "forecast 6.1",
+        accuracy = 0.68,  # needs to come from daily analysis.
+        performance = as.numeric(elapsed_time)  # elapsed seconds.
+      ),
+      results = FillTimes(as.data.frame(forecast_bikes))
+    )
+    return(output)
 
-  else {
-    
+  } else {
+
     #
     # Testing the model.
     #
@@ -61,7 +87,7 @@ FitArimaModel <- function(data=NULL, production_model=FALSE, station_id=445, for
     bikes_arima <- auto.arima(train_data)
     forecast_bikes <- forecast.Arima(bikes_arima, h=forecast_minutes)
     arima_rmse <- accuracy(forecast_bikes, test_data)[,2]
-    
+
     cat(' done.\n')
     return(data.frame(arima_rmse))
   }
